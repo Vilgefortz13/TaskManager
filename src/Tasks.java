7414ef6a45b5e2ff6ab4e823bdf7c24f8c39f9c6
Rinat Uzanzaev <rinatuzanzaev13@gmail.com>
2023-09-04 22:05:20 +0300

    added constructor to Tasks

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,4 +1,9 @@
 public class Tasks {
     private String description;
     private boolean isComplete;
+
+    public Tasks(String description, boolean isComplete) {
+        this.description = description;
+        this.isComplete = isComplete;
+    }
 }
